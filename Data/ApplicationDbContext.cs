8aaaea561744f98a25963e5fb7c13b0e4aaa2df3
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-09-09 11:56:50 +0300

    в папке Data создал ApplicationDbContext.cs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; } 
+        public DbSet<Repairer> Repairers { get; set; }
+        public DbSet<Customer> Customers { get; set; } 
+    }
+
+}
